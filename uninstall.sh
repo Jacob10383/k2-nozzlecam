9c3fa38389e713ec1e1a8a3f7c4cef723c8790b7
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-07-27 20:18:55 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,28 +4,31 @@ set -e
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
-# return original auto_uvc if backup exists
-if [ -e /usr/bin/auto_uvc.sh.bak ] && [ -e /usr/bin/auto_uvc.sh ]; then
-    rm -rf /usr/bin/auto_uvc.sh
-	cp -p /usr/bin/auto_uvc.sh.bak /usr/bin/auto_uvc.sh
-	rm -rf /usr/bin/auto_uvc.sh.bak
+# remove the installed file/symlink and restore original auto_uvc if backup exists
+if [ -e /usr/bin/auto_uvc.sh.bak ]; then
+    rm -f /usr/bin/auto_uvc.sh
+    mv /usr/bin/auto_uvc.sh.bak /usr/bin/auto_uvc.sh
 fi
 
-
-# return original 60-v4l if backup exists
-if [ -e /etc/hotplug.d/usb/60-v4l.bak ] && [ -e /etc/hotplug.d/usb/60-v4l ]; then
-    rm -rf /etc/hotplug.d/usb/60-v4l
-	cp -p /etc/hotplug.d/usb/60-v4l.bak /etc/hotplug.d/usb/60-v4l
-	rm -rf /etc/hotplug.d/usb/60-v4l.bak
+# remove the installed file/symlink and restore original 60-v4l if backup exists
+if [ -e /etc/hotplug.d/usb/60-v4l.bak ]; then
+    rm -f /etc/hotplug.d/usb/60-v4l
+    mv /etc/hotplug.d/usb/60-v4l.bak /etc/hotplug.d/usb/60-v4l
 fi
 
 # remove the old symlinks
-rm -rf /usr/bin/ustreamer
-rm -rf /mnt/UDISK/printer_data/config/custom/v4lctls.cfg
-rm -rf /usr/share/klipper/klippy/extras/gcode_shell_command.py
+rm -f /usr/bin/ustreamer
+rm -f /usr/share/klipper/klippy/extras/gcode_shell_command.py
 
-# remove the v4lctls macro file
-python ${SCRIPT_DIR}/ensure_included.py \
+# remove the v4lctls.cfg file
+rm -f /mnt/UDISK/printer_data/config/custom/v4lctls.cfg
+
+# remove the init.d ustream script and config folder
+rm -rf /etc/ustreamer
+rm -f /etc/init.d/ustreamer
+
+# remove the v4lctls macro file alignment
+python "${SCRIPT_DIR}/ensure_included.py" \
     ~/printer_data/config/custom/main.cfg v4lctls.cfg --remove
 
 echo "Uninstallation complete. reboot the system to apply changes."
