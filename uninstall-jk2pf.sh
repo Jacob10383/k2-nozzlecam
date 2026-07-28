e26d48a6163fb5db75feb3bd08656905967184e1
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-07-28 11:33:07 -0400

    Update uninstall-jk2pf.sh

--- uninstall-jk2pf.sh
+++ uninstall-jk2pf.sh
@@ -10,8 +10,8 @@ rm -f /mnt/UDISK/printer_data/config/v4lctls.cfg
 # remove the 3docam.env file
 rm -f /etc/ustreamer/3docam.env
 
-# remove the v4lctls gcode macro line from overrides.cfg
+# remove the 3dov4lctls gcode macro line from overrides.cfg
 python "${SCRIPT_DIR}/ensure_included.py" \
-    ~/printer_data/config/overrides.cfg v4lctls.cfg --remove
+    ~/printer_data/config/overrides.cfg 3dov4lctls.cfg --remove
 
 echo "Uninstallation complete. reboot the system to apply changes."
