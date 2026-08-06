794188b393c0ffc6a96a2980a2f05eae560e406a
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-08-06 09:54:10 -0400

    Updated install-jk2pf.sh
    added missing command to copy macro file

--- install-jk2pf.sh
+++ install-jk2pf.sh
@@ -13,6 +13,9 @@ systemctl start ustreamer@3docam
 # Enable the service to start automatically on system boot
 systemctl enable ustreamer@3docam
 
+# copy the 3dov4lctls.cfg to /mnt/UDISK/printer_data/config/3dov4lctrls.cfg
+cp -f "${SCRIPT_DIR}/v4lctls.cfg" /mnt/UDISK/printer_data/config/3dov4lctls.cfg
+
 # add the macro 3dov4lctls.cfg into the printer.cfg file
 python "${SCRIPT_DIR}/ensure_included.py" \
     ~/printer_data/config/overrides.cfg 3dov4lctls.cfg
